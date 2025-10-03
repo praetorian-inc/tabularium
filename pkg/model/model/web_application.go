563aead4d83607cde35810fd4c47c59a668f7934
EvanLeleux <85642615+EvanLeleux@users.noreply.github.com>
2025-10-02 19:41:52 -0500

    Add Application surface (#74)
    * add application surface
    
    * add isPrivate

--- pkg/model/model/web_application.go
+++ pkg/model/model/web_application.go
@@ -11,6 +11,7 @@ import (
 	"slices"
 
 	"github.com/praetorian-inc/tabularium/pkg/lib/normalize"
+	"github.com/praetorian-inc/tabularium/pkg/model/attacksurface"
 	"github.com/praetorian-inc/tabularium/pkg/registry"
 )
 
@@ -112,6 +113,7 @@ func (w *WebApplication) GetHooks() []registry.Hook {
 func (w *WebApplication) Defaulted() {
 	w.BaseAsset.Defaulted()
 	w.Class = "webapplication"
+	w.AttackSurface = []string{string(attacksurface.Application)}
 	if w.URLs == nil {
 		w.URLs = []string{}
 	}
@@ -145,6 +147,10 @@ func (w *WebApplication) Identifier() string {
 
 func (w *WebApplication) IsSeed() bool {
 	return slices.Contains(w.GetLabels(), SeedLabel)
+}
+
+func (w *WebApplication) IsPrivate() bool {
+	return false
 }
 
 func (w *WebApplication) Merge(other Assetlike) {
