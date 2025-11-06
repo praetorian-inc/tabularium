576785f0c0a53ec95abf6676c1709dc75f3eecd5
Noah Tutt <69209160+noah-tutt-praetorian@users.noreply.github.com>
2025-11-06 17:08:29 -0500

    Merge pull request #98 from praetorian-inc/nmt/retries
    nmt/retries

--- pkg/model/model/job.go
+++ pkg/model/model/job.go
@@ -30,6 +30,7 @@ type Job struct {
 	CredentialIDs         []string          `dynamodbav:"credential_ids" json:"credential_ids,omitempty" desc:"List of credential IDs to retrieve and inject into job execution." example:"[\"550e8400-e29b-41d4-a716-446655440000\"]"`
 	LargeArtifactFileName string            `dynamodbav:"large_artifact_filename" json:"large_artifact_filename,omitempty" desc:"The name of the file that contains the large artifacts." example:"large_artifact.zip"`
 	S3DownloadURL         string            `dynamodbav:"s3DownloadURL" json:"s3DownloadURL,omitempty" desc:"The URL of the file that contains the large output." example:"https://s3.amazonaws.com/big_output.zip"`
+	Retries               int               `dynamodbav:"retries" json:"retries" desc:"The number of times this job has been retried."`
 	AllowRepeat           bool              `dynamodbav:"allowRepeat" json:"allowRepeat" desc:"Indicates if repeating this job should be allowed. Used for manual jobs, or rescan jobs, that should not block other job executions." example:"false"`
 	Full                  bool              `dynamodbav:"-" json:"full,omitempty" desc:"Indicates if this is a full scan job." example:"false"`
 	Capabilities          []string          `dynamodbav:"-" json:"capabilities,omitempty" desc:"List of specific capabilities to run for this job." example:"[\"portscan\", \"nuclei\"]"`
