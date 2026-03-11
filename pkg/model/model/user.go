8b885f698e76e1ed9f3aec0111bf54bd67955fb9
Peter Kwan <peter.kwan@praetorian.com>
2026-03-11 12:14:38 -0700

    Added the Sub field to the user model

--- pkg/model/model/user.go
+++ pkg/model/model/user.go
@@ -15,10 +15,13 @@ type User struct {
 	// returns min(accountRole, RoleCeiling).
 	RoleCeiling Role
 
+	// Authentication information:
 	// Auth method (e.g. SSO, API key, username/password)
 	AccessType ChariotAccessType
 	// SSO domain (e.g. "customer-domain.com")
 	SSODomain string
+	// Cognito sub
+	Sub string
 }
 
 func (u *User) Linked(username string) bool {
